68842a01bb1c2b6cf89e13795e158a54124bc81d
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-08-23 10:58:57 +0530

    modified index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import './style.css';
 import "./index.css";
+import React from "react";
 
 ReactDOM.render(
     <App />,document.getElementById("root")
