134454a38599c34dbb07d95c5e0f98a31a1103df
Saiprabhu007 <saiprabhu.dandanayak@zemosolabs.com>
2024-04-05 16:10:44 +0530

    ✨ feat : implemented absolute routing

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,12 +2,8 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
-const portfinder = require('portfinder');
 
-module.exports = async () => {
-  const port = await portfinder.getPortPromise({ port: 3000 });
-
-  return {
+module.exports =   {
     mode: 'development',
     entry: path.resolve(__dirname, 'src/index.tsx'),
     output: {
@@ -16,6 +12,19 @@ module.exports = async () => {
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, 'src','components'),
+        '@atoms': path.resolve(__dirname, 'src/components/atoms'),
+        '@molecules': path.resolve(__dirname, 'src/components/molecules'),
+        '@assets': path.resolve(__dirname, 'public','assets'),
+        '@utils': path.resolve(__dirname, 'src','utils'),
+        '@theme':path.resolve(__dirname, 'src','theme'),
+        '@mocks': path.resolve(__dirname, 'src', 'mocks'),
+        '@services':path.resolve(__dirname, 'src', 'services'),
+        '@pages':path.resolve(__dirname, 'src', 'pages'),
+        '@routes':path.resolve(__dirname, 'src', 'routes'),
+      },
     },
     devtool: 'inline-source-map',
     module: {
@@ -49,9 +58,8 @@ module.exports = async () => {
     ],
     devServer: {
       host: 'localhost',
-      port: port,
+      port: 3000,
       historyApiFallback: true,
       open: true,
     },
-  };
-};
+  }
